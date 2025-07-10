9b4c211c0a177c5bd1c1e0a2ffaa19cea3acf219
Дмитрий Трегубов <diamonbusiness@gmail.com>
2025-07-10 20:37:13 +0300

    исправлена ошибка в урл

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -8,8 +8,8 @@ from content.views import CategoriesViewSet, GenreViewSet, TitleViewSet
 api_v1_router = DefaultRouter()
 # тут регистрируй эндпоинты
 api_v1_router.register(r'categories', CategoriesViewSet, basename='categories')
-api_v1_router.register(r'genre', GenreViewSet, basename='genres')
-api_v1_router.register(r'title', TitleViewSet, basename='titles')
+api_v1_router.register(r'genres', GenreViewSet, basename='genres')
+api_v1_router.register(r'titles', TitleViewSet, basename='titles')
 
 urlpatterns = [
     path('v1/', include('djoser.urls')),
