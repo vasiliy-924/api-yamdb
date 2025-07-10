a1cd09e15e2cd4a48128297561cb02bd4b5d1c69
Дмитрий Трегубов <diamonbusiness@gmail.com>
2025-07-10 19:19:58 +0300

    добавлены Viewsets и serializers для Categories,Genre,Title

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from users.models import User
 import jwt
 from django.conf import settings
+from content.models import Categories, Genre, Title
+
 
 class TokenObtainSerializer(serializers.Serializer):
     username = serializers.CharField()
@@ -15,3 +17,23 @@ class TokenObtainSerializer(serializers.Serializer):
             raise serializers.ValidationError('Неверный username или confirmation_code')
         token = jwt.encode({'username': user.username}, settings.SECRET_KEY, algorithm='HS256')
         return {'token': token}
+
+
+class CategoriesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Categories
+        fields = ('name', 'slug')
+        lookup_field = 'slug'
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ('name', 'slug')
+        lookup_field = 'slug'
+
+
+class TitleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Title
+        fields = '__all__'
