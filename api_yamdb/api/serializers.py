2ce655665479618c8c931df2fb6c6d6bdc29ee7d
Kirill Domnikov <dikey1993@yandex.ru>
2025-07-11 01:44:22 +0300

    feature/reviews-comments-piece-rating 1 ver.

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -3,6 +3,8 @@ from users.models import User
 import jwt
 from django.conf import settings
 from content.models import Categories, Genre, Title
+from reviews.models import Comment, Review
+from rest_framework.validators import UniqueTogetherValidator
 
 
 class TokenObtainSerializer(serializers.Serializer):
@@ -37,3 +39,39 @@ class TitleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Title
         fields = '__all__'
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    """Сериализатор для отзывов."""
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True,
+        default=serializers.CurrentUserDefault()
+    )
+    score = serializers.IntegerField(min_value=1, max_value=10)
+
+    class Meta:
+        model = Review
+        fields = ('id', 'text', 'author', 'score', 'pub_date')
+        read_only_fields = ('id', 'author', 'pub_date')
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Review.objects.all(),
+                fields=('title', 'author'),
+                message='Вы уже оставили отзыв на это произведение'
+            )
+        ]
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    """Сериализатор для комментариев."""
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True,
+        default=serializers.CurrentUserDefault()
+    )
+
+    class Meta:
+        model = Comment
+        fields = ('id', 'text', 'author', 'pub_date')
+        read_only_fields = ('id', 'author', 'pub_date')
---
+++ api_yamdb/reviews/signals.py
@@ -0,0 +1,14 @@
+from django.db.models import Avg
+from django.db.models.signals import post_delete, post_save
+from django.dispatch import receiver
+
+from .models import Review
+
+
+@receiver([post_save, post_delete], sender=Review)
+def update_rating(sender, instance, **kwargs):
+    """Обновляет рейтинг произведения по оценкам отзывов."""
+    title = instance.title
+    avg_score = title.reviews.aggregate(Avg('score'))['score__avg']
+    title.rating = round(avg_score) if avg_score else None
+    title.save(update_fields=['rating'])
