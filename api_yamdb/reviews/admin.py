bd094680fa78e43ad00904ac2f0d69b9167e6f7c
Vasiliy Petrov <vasiliy@vppetrov.ru>
2025-07-14 10:04:04 +0300

    add admin.py to users, content and reviews

---
+++ api_yamdb/reviews/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from .models import Review, Comment
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'author', 'score', 'pub_date')
+    search_fields = ('text', 'author__username', 'title__name')
+    list_filter = ('score', 'pub_date')
+    date_hierarchy = 'pub_date'
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'review', 'author', 'pub_date')
+    search_fields = ('text', 'author__username', 'review__id')
+    list_filter = ('pub_date',)
+    date_hierarchy = 'pub_date' 
---
+++ api_yamdb/users/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'role', 'is_superuser')
+    list_filter = ('role', 'is_superuser')
+    search_fields = ('username', 'email') 
