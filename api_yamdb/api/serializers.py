802967d54b9a402bb1b0aebc8c0ac8f1dca17ce8
Vasiliy Petrov <vasiliy@vppetrov.ru>
2025-07-14 21:27:02 +0300

    updated TitleSerializer

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -160,12 +160,15 @@ class TitleSerializer(serializers.ModelSerializer):
 
     rating = serializers.IntegerField(read_only=True)
     category = serializers.SlugRelatedField(
-        slug_field='slug', queryset=Category.objects.all(),
-        write_only=True
+        slug_field='slug',
+        queryset=Category.objects.all(),
+        required=True
     )
     genre = serializers.SlugRelatedField(
-        slug_field='slug', queryset=Genre.objects.all(), many=True,
-        write_only=True
+        slug_field='slug',
+        queryset=Genre.objects.all(),
+        many=True,
+        required=True
     )
     category_detail = CategorySerializer(source='category', read_only=True)
     genre_detail = GenreSerializer(source='genre', many=True, read_only=True)
