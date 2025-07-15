6d4f5332cefc3cbe23eedfc70a59c8c35b68efca
Vasiliy Petrov <vasiliy@vppetrov.ru>
2025-07-15 22:41:03 +0300

    sorted imports

--- api_yamdb/users/admin.py
+++ api_yamdb/users/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from .models import User
+
+from users.models import User
 
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
+    """
+    Админка для модели User с расширенными возможностями
+    отображения и управления.
+    """
     list_display = (
         'username',
         'email',
@@ -79,16 +84,20 @@ class UserAdmin(BaseUserAdmin):
 
     @admin.display(description='Кол-во комментариев')
     def comments_count(self, obj):
+        """Возвращает количество комментариев пользователя."""
         return obj.comments.count()
 
     @admin.display(description='Кол-во отзывов')
     def reviews_count(self, obj):
+        """Возвращает количество отзывов пользователя."""
         return obj.reviews.count()
 
     @admin.action(description='Активировать выбранных пользователей')
     def make_active(self, request, queryset):
+        """Массово активирует выбранных пользователей."""
         queryset.update(is_active=True)
 
     @admin.action(description='Деактивировать выбранных пользователей')
     def make_inactive(self, request, queryset):
+        """Массово деактивирует выбранных пользователей."""
         queryset.update(is_active=False)
--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import AbstractUser
-from django.db import models
-from django.core.validators import RegexValidator
 from django.core.exceptions import ValidationError
+from django.core.validators import RegexValidator
+from django.db import models
 
 
 class User(AbstractUser):
